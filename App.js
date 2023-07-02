118e8efb6eedbaa163f93d3136f3a959a901dbe0
happybanwal <himanshubanwal2001@gmail.com>
2023-07-02 11:08:31 +0530

    tailwind done

--- App.js
+++ App.js
@@ -3,18 +3,10 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text> puneet Open up App.js to start working on your app!</Text>
+    <View className={"flex-1 justify-center items-center"}>
+      <Text className={''}> puneet </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
